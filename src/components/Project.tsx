82444951f5949a8a8f327d6a5911972282655c23
gastoncarrizo12 <gcarrizochaio@gmail.com>
2025-02-28 16:57:56 -0300

    todo listo

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import mock10 from '../assets/images/mock10.png';
+import bookcloud from '../assets/images/bookcloud.onrender.com_.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -8,8 +8,8 @@ function Project() {
         <h1>Proyectos Personales</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Bookcloud</h2></a>
+                <a href="https://bookcloud.onrender.com/" target="_blank" rel="noreferrer"><img src={bookcloud} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://bookcloud.onrender.com/" target="_blank" rel="noreferrer"><h2>Bookcloud</h2></a>
                 <p>Developed movie finder app with semantic search and sentiment analysis using OpenAI GPT-3.5 Turbo, Qdrant, React, and Flask.</p>
             </div>
         </div>
