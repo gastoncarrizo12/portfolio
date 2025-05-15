94a4a1fe7327e191d0bec8a73c4430904a9881e2
gastoncarrizo12 <gcarrizochaio@gmail.com>
2025-05-15 14:59:01 -0300

    agregue boton Ver Proyecto

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -25,20 +25,42 @@ function Project() {
   };
 
   return (
+
     <div className="projects-container" id="projects">
       <h1>Proyectos Personales</h1>
       <div className="projects-grid">
-        <div className="project">
-            <div className="carousel-container">
-              <button className="arrow left" onClick={handlePrev}>❮</button>
-              <img src={images[currentImage]} className="carousel-image zoom" alt="thumbnail" width="100%" />
-              <button className="arrow right" onClick={handleNext}>❯</button>
-            </div>
-          <a href="https://bookcloud.onrender.com/" target="_blank" rel="noreferrer"><h2>Bookcloud</h2></a>
-          <p>Bookcloud es una aplicación web MERN diseñada para la gestión y venta de libros.Utiliza MongoDB Atlas para el almacenamiento escalable de datos, integra MercadoPago para pagos seguros y Cloudinary para la gestión optimizada de imágenes. La plataforma está alojada en Render, garantizando un rendimiento robusto. Además, cuenta con secciones de "Destacados" y "Recomendados" que facilitan el descubrimiento de libros, ofreciendo una experiencia de usuario intuitiva y atractiva.
-          Cuenta con un sistema de Administrador, el cual puede crear, leer, editar y eliminar libros.</p>
-          <a href="https://github.com/gastoncarrizo12/bookcloud-server" target="_blank" rel="noreferrer">
-              🔗 Código fuente
+                <div className="project">
+          <div className="carousel-container">
+            <button className="arrow left" onClick={handlePrev}>❮</button>
+            <img src={images[currentImage]} className="carousel-image zoom" alt="thumbnail" width="100%" />
+            <button className="arrow right" onClick={handleNext}>❯</button>
+          </div>
+          <div className="project-title-container">
+            <h2>
+              <a href="https://bookcloud.onrender.com/" target="_blank" rel="noreferrer">
+                Bookcloud
+              </a>
+            </h2>
+            <a 
+              href="https://bookcloud.onrender.com/" 
+              className="btn-view" 
+              target="_blank" 
+              rel="noreferrer"
+            >
+              Ver Proyecto
+            </a>
+          </div>
+
+          <p>
+            Bookcloud es una aplicación web MERN diseñada para la gestión y venta de libros.Utiliza MongoDB Atlas para el almacenamiento escalable de datos, integra MercadoPago para pagos seguros y Cloudinary para la gestión optimizada de imágenes. La plataforma está alojada en Render, garantizando un rendimiento robusto. Además, cuenta con secciones de "Destacados" y "Recomendados" que facilitan el descubrimiento de libros, ofreciendo una experiencia de usuario intuitiva y atractiva.
+            Cuenta con un sistema de Administrador, el cual puede crear, leer, editar y eliminar libros.
+          </p>
+          <a 
+            href="https://github.com/gastoncarrizo12/bookcloud-server" 
+            target="_blank" 
+            rel="noreferrer"
+          >
+            🔗 Código fuente
           </a>
         </div>
         <div className="project">
@@ -47,12 +69,19 @@ function Project() {
               <img src={UrlShortener} alt="thumbnail" width="100%" />
               <button className="arrow right" onClick={handleNext}>❯</button>
             </div>
-          <a href="https://gcch.netlify.app/" target="_blank" rel="noreferrer"><h2>URL Shortener</h2></a>
+          
+          <div className="project-header">
+          <h2>URL Shortener</h2>
+          <a className="btn-view" href="https://gcch.netlify.app/" target="_blank" rel="noreferrer">
+              Ver Proyecto
+          </a>
+          </div>
           <p>Este proyecto es una aplicación de acortador de URLs desarrollada con el stack MERN (MongoDB, Express.js, React, Node.js). Permite a los usuarios generar enlaces cortos personalizados o automáticos, almacenar y gestionar sus URLs de forma segura y sencilla.</p>
           <a href="https://github.com/gastoncarrizo12/URL-Shortener" target="_blank" rel="noreferrer">
               🔗 Código fuente
           </a>
         </div>
+        
       </div>
     </div>
   );
